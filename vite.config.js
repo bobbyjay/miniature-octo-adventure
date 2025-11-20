20fadb98fe6e2469581921ea582f7b6cbdacb855
bobbyjay <jaymuhammed401@gmail.com>
2025-11-20 06:40:07 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // optional (default is 5173)
-  },
-  define: {
-    "process.env": {}, // Fixes some libraries expecting process.env
+    proxy: {
+      "/api": {
+        target: "https://clutchden.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
