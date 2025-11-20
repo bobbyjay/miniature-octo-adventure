aac3cec27f1ae61a7fe91efa1521225e122e2220
bobbyjay <jaymuhammed401@gmail.com>
2025-11-19 21:44:20 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/HomePage";
 import Login from "./pages/LoginPage";
 import Signup from "./pages/RegisterPage";
@@ -6,21 +7,19 @@ import Dashboard from "./pages/DashboardPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
 
-        {/* Login */}
-        <Route path="/signin" element={<Login />} />
-        <Route path="/login" element={<Login />} /> {/* fallback */} 
+      {/* Login */}
+      <Route path="/signin" element={<Login />} />
+      <Route path="/login" element={<Login />} />
 
-        {/* Signup / Register (both work now) */}
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/register" element={<Signup />} /> {/* fixes your error */}
+      {/* Signup / Register */}
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/register" element={<Signup />} />
 
-        {/* Dashboard */}
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+      {/* Dashboard */}
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
