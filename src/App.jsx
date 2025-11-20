7cbfca4676ef213dc4c1e5332b608725dd46011f
bobbyjay <jaymuhammed401@gmail.com>
2025-11-19 21:14:05 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Login from "./pages/LoginPage";
-import Signup from "./pages/SignupPage";
+import Signup from "./pages/RegisterPage";
 import Dashboard from "./pages/DashboardPage";
 
 export default function App() {
